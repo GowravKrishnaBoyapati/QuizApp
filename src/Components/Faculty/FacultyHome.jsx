567762f8e212fac9c50dda7d07042b4aef8514b4
gowrav143 <gowrav143.krishna@gmail.com>
2023-07-08 18:52:01 -0400

    Second Commit

---
+++ src/Components/Faculty/FacultyHome.jsx
@@ -0,0 +1,168 @@
+import React, { useEffect, useState } from 'react'
+import withAuthorization from "../withAuthorization";
+import AddCardIcon from '@mui/icons-material/AddCard';
+import { Link } from 'react-router-dom';
+import { db, auth } from '../../firebase'
+import * as routes from '../../constants/routes'
+import './FacultyHome.css'
+import { styled } from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
+import ResultGraph from './ResultGraph';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import ReactModal from 'react-modal';
+import AddQuiz from './AddQuiz';
+
+const IOSSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+        opacity: 1,
+        border: 0,
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+    },
+    '&.Mui-focusVisible .MuiSwitch-thumb': {
+      color: '#33cf4d',
+      border: '6px solid #fff',
+    },
+    '&.Mui-disabled .MuiSwitch-thumb': {
+      color:
+        theme.palette.mode === 'light'
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
+    },
+    '&.Mui-disabled + .MuiSwitch-track': {
+      opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    width: 22,
+    height: 22,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 26 / 2,
+    backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}));
+
+function FacultyHome() {
+
+  const [quizData, setQuizData] = useState([])
+  const [activeQuiz, setActiveQuiz] = useState()
+  const [quizAttempts, setQuizAttempts] = useState([])
+  const [addQuizModal,setAddQuizModal] = useState(false)
+  const [EditQuizModal,setEditQuizModal] = useState(false)
+
+  const doUpdate =(id,status)=>{
+    console.log(activeQuiz)
+    let data = activeQuiz.data
+    db.doQuizStatus_Enable_Disable(id, status).then(()=>{
+      data['disabled']=!status;
+      console.log(data)
+    setActiveQuiz({...activeQuiz, data: data})})
+  }
+  useEffect(() => {
+    db.doTotalQuizesData().onSnapshot((snapshot) => {
+      setQuizData(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+    })
+  }
+    , [])
+
+  useEffect(() => { console.log(quizData) }, [quizData])
+  useEffect(() => {
+    console.log('Active Quiz Object: ', activeQuiz); 
+    db.doGetQuizAttemps(activeQuiz?.id).then((snapshot) => {
+      setQuizAttempts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+    })
+  }, [activeQuiz])
+
+  return (
+    <div className="faculty-home">
+      <div className="faculty-quizes">
+        <div className='faculty-quiz-heading'>
+          <h1>All Quizes </h1>
+        </div>
+        <div className="add-quiz-faculty">
+          <h3 style={{ textDecoration: 'none' }} onClick={()=>{setAddQuizModal(true)}} >Add Quiz <AddCardIcon style={{ fontSize: 'inherit' }} /></h3>
+          <ReactModal
+          isOpen={addQuizModal}
+          contentLabel="Example Modal"
+          onRequestClose={()=>setAddQuizModal(false)}
+          className='add-quiz-modal'
+          overlayClassName="add-quiz-modal-overlay"
+          >
+            <button onClick={()=>{setAddQuizModal(false)}} > X </button>
+            <AddQuiz setIsOpen={(e)=>{setAddQuizModal(e)}} />
+          </ReactModal>
+          <ReactModal
+          isOpen={EditQuizModal}
+          contentLabel="Example Modal"
+          onRequestClose={()=>setEditQuizModal(false)}
+          className='add-quiz-modal'
+          overlayClassName="add-quiz-modal-overlay"
+          >
+            <button onClick={()=>{setEditQuizModal(false)}} > X </button>
+            <AddQuiz setIsOpen={(e)=>{setEditQuizModal(e)}} />
+          </ReactModal>
+        </div>
+        <div className="faculty-quiz-set "  >
+          {
+            quizData.map((i,index) => (
+              <div className={'quizes-section faculty-quizes-section ' + ((activeQuiz == i || index==0) ? 'faculty-active-quiz' : '')} onClick={() => { console.log('Qid: ', i.id); setActiveQuiz(i) }}>
+                <div className="quiz-title">
+                  <div className="main-title"><h3>{i.data['title']}</h3></div>
+                  <div className="max-mark">max-mark: {i.data['max_mark']}</div>
+                </div>
+                <div style={{ display: 'flex' }}>
+                  <button className="take-quiz" onClick={()=>{setAddQuizModal(true)}}>Edit <AppRegistrationIcon /></button>
+                  <button className="take-quiz" onClick={(e)=>{confirm('Do you want to delete the Quiz?')?db.doDelteQuiz(i.id):''}} >Delete <DeleteForeverIcon/></button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+
+      {activeQuiz!=undefined && <div className="faculty-home-admin">
+        <div className="quiz-title">
+          <div className="main-title" style={{ margin: 'auto', marginBottom: '10px' }}><h3 style={{ filter: 'none' }}>Title : {activeQuiz?.data?.title} [ max mark: {activeQuiz?.data?.max_mark} ]</h3></div>
+        </div>
+        <ul className="faculty-quiz-props">
+          <li style={{ display: 'flex', justifyContent: 'space-evenly', margin: 'auto', textAlign: 'center' }}>
+            <h5 style={{ margin: 'auto' }}>Quiz Status</h5>
+            <IOSSwitch sx={{ m: 2 }} checked={!activeQuiz?.data?.disabled} onClick={(e) => { console.log(activeQuiz?.id); confirm('Do want to ' + (activeQuiz?.data?.disabled? 'Enable' : 'Disable') + ' the '+ activeQuiz?.data?.title +'?') == true ? doUpdate(activeQuiz?.id, activeQuiz?.data?.disabled) : '' }} />
+          </li>
+          <li style={{ display: 'flex', justifyContent: 'space-evenly', margin: 'auto', textAlign: 'center' }}>
+            <h5 style={{ margin: 'auto' }}>Attempts : {quizAttempts.length}</h5>
+          </li>
+        </ul>
+        <div className="faculty-quiz-results">
+          <ResultGraph data={quizAttempts} quiz={activeQuiz?.data} />
+        </div>
+      </div>}
+    </div>
+  )
+}
+
+const authCondition = authUser => !!authUser;
+
+export default withAuthorization(authCondition)(FacultyHome);
---
+++ src/firebase/db.jsx
@@ -0,0 +1,70 @@
+//this is going to store Firebase realtime database API code
+import { db } from "./firebase";
+
+//Get All Quizes%
+export const allQuizes = () => db.collection('Quizes').get()
+
+//Get Questions of a particular Quiz using Quiz ID
+export const getQuizQuestions  = (id) => db.collection('Quizes').doc(id).collection('Questions').get()
+
+//Create an User
+export const doCreateUser = (id, username, email) =>
+  db.collection('users').doc(id).set({
+    username,
+    email,
+    verified:false,
+    h_score:0,
+    t_quizes:0,
+
+  });
+
+//returns all users from firebase realtime db
+export const onceGetUsers = () => db.collection('users').get();
+
+//Get an user using UID
+export const doGetAnUnser = uid => db.collection('users').doc(uid).get();
+
+//Get user quizes attempts
+export const doGetUserQuizAttempts = (uid) => db.collection('users').doc(uid).collection('quizes').get();
+
+//Update the status of quiz, parameters: Status - status of quiz; UID -> user ID; qid -> Quiz ID; score -> score obtained in quiz; t_score -> total score
+export const doQuizStatusUpdate = (status,uid,qid,score,t_score,title) => {
+  db.collection('users').doc(uid).collection('quizes').doc(qid).set({
+    "score": score,
+    "status":status,
+    "title":title,
+    "t_score": t_score
+  }).then(()=>console.log('Updated User records for quizes!'));
+  db.collection('Quizes').doc(qid).collection('attempts').doc(uid).set({"score": score,
+  "status":status}).then(()=>console.log('Updated Quiz attempt records!'));
+}
+
+//-------------Faculty Quieris------------------
+
+//Create a quiz with the given title
+export const doCreateQuiz = (title) => db.collection('Quizes').doc().set({
+  'title':title,
+  'disabled':true,
+  'max_mark':0
+})
+
+//Get All Quizes Data
+export const doTotalQuizesData = () => db.collection('Quizes')
+
+//Delete full quiz
+export const doDelteQuiz = (qid) => db.collection('Quizes').doc(qid).delete().then(() => {
+ 
+}).catch((error) => {
+  alert("Error removing Quiz: ", error);
+});
+
+//Accessing the Users Attempts Data from a specific quiz using the unique quiz identifier
+export const doGetQuizAttemps = (qid) => db.collection('Quizes').doc(qid).collection('attempts').get()
+
+//Updating quiz status --> Enabled or Disabled
+export const doQuizStatus_Enable_Disable = (qid,status) => db.collection('Quizes').doc(qid).update({disabled: !status})
+
+//Editing Quizes using unique quiz identifier and the unique question identifier
+export const doUpdateQuizQuestions = (uqid,qid,data) => db.collection('Quizes').doc(uqid).collection('Question').doc(qid).set(data)
+
+// other APIs could come below
